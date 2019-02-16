424017d1277dd0c9455658d785a6b3651764262d
Nik Kantar <nik@nkantar.com>
2019-02-15 17:06:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="parsenvy",
-    version="2.0.7",
+    version="2.0.8",
     author="Nik Kantar",
     author_email="nik@nkantar.com",
     description="Enviously elegant environment variable parsing",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/nkantar/Parsenvy",
     packages=setuptools.find_packages(),
 )
