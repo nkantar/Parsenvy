e1eec87220da0420a43424d3ddd7a6c54ced71b0
Nik Kantar <nik@nkantar.com>
2021-02-20 21:54:18 -0800

    Rename function param for clarity

--- parsenvy/parsenvy.py
+++ parsenvy/parsenvy.py
@@ -52,88 +52,99 @@ def default_if_none(func: Callable[[builtins.str, Any], Any]) -> Optional[Any]:
 
 @default_if_none
 def bool(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[builtins.bool] = None,
 ) -> Optional[builtins.bool]:
-    if value.lower() in TRUES:
+    """
+    Parse environment variable value into a boolean.
+
+    Args:
+        env_var (str): Name of desired environment variable.
+        default (bool, optional): Optional fallback value.
+
+    Returns:
+        bool (optional): Environment variable typecast into a boolean.
+
+    """
+    if env_var.lower() in TRUES:
         return True
 
-    if value.lower() in FALSES:
+    if env_var.lower() in FALSES:
         return False
 
     raise ValueError(
-        f"Invalid boolean value specified: {value} "
+        f"Invalid boolean value specified: {env_var} "
         "Parsenvy accepts 'true', '1', 'false', and '0' as boolean values."
     )
 
 
 @default_if_none
 def int(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[builtins.int] = None,
 ) -> Optional[builtins.int]:
     try:
-        return builtins.int(value)
+        return builtins.int(env_var)
     except ValueError:
         raise TypeError(
-            f"Invalid integer value specified: {value} "
+            f"Invalid integer value specified: {env_var} "
             "Parsenvy accepts only valid integers as integer values."
         )
 
 
 @default_if_none
 def float(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[builtins.float] = None,
 ) -> Optional[builtins.float]:
     try:
-        return builtins.float(value)
+        return builtins.float(env_var)
     except ValueError:
         raise TypeError(
-            f"Invalid float value specified: {float} "
+            f"Invalid float value specified: {env_var} "
             "Parsenvy accepts only valid floats and integers as float values"
         )
 
 
 @default_if_none
 def list(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[List[Any]] = None,
 ) -> Optional[List[Any]]:
-    if value == "":
+    if env_var == "":
         return default
 
-    return value.split(",")
+    return env_var.split(",")
 
 
 @default_if_none
 def tuple(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[Tuple[Any, ...]] = None,
 ) -> Optional[Tuple[Any, ...]]:
-    if value == "":
+    if env_var == "":
         return default
 
-    return builtins.tuple(value.split(","))
+    return builtins.tuple(env_var.split(","))
 
 
 @default_if_none
 def str(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[builtins.str] = None,
 ) -> Optional[builtins.str]:
-    if value == "":
+    if env_var == "":
         return default
 
-    return value
+    return env_var
 
 
 @default_if_none
 def set(
-    value: builtins.str,
+    env_var: builtins.str,
     default: Optional[Set[Any]] = None,
 ) -> Optional[Set[Any]]:
-    if value is None or value == "":
+    if env_var is None or env_var == "":
         return default
 
-    return builtins.set(value.split(","))
+    return builtins.set(env_var.split(","))
