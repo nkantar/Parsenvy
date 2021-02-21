9aa04857e06f01a4e74812d496038187fa6bf6cf
Nik Kantar <nik@nkantar.com>
2021-02-20 19:52:05 -0800

    Remove extraneous whitespace from tests

--- tests/test_default_if_none.py
+++ tests/test_default_if_none.py
@@ -17,7 +17,6 @@ def func():
 def test_default_if_none_arg(func, monkeypatch):
     """Desired env var is set."""
     monkeypatch.setenv("foo", "bar")
-
     decorated_func = parsenvy.default_if_none(func)
     assert decorated_func("foo") == "bar"
 
@@ -25,7 +24,6 @@ def test_default_if_none_arg(func, monkeypatch):
 def test_default_if_none_default(func, monkeypatch):
     """Desired env var isn't set, default is supplied."""
     monkeypatch.delenv("foo", raising=False)
-
     decorated_func = parsenvy.default_if_none(func)
     assert decorated_func("foo", "bar") == "bar"
 
@@ -33,6 +31,5 @@ def test_default_if_none_default(func, monkeypatch):
 def test_default_if_none_neither(func, monkeypatch):
     """Desired env var isn't set, default isn't supplied."""
     monkeypatch.delenv("foo", raising=False)
-
     decorated_func = parsenvy.default_if_none(func)
     assert decorated_func("foo") is None
