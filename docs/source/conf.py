7082f7ce041f6f24bddb22066370aecbc2123652
Nik Kantar <nik@nkantar.com>
2024-01-01 12:03:46 -0800

    Add sphinx-mdinclude

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
+    "sphinx_mdinclude",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
